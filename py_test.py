2516a8c622b28c5ca4d8d35f33d0e43505038545
runtimeterrorss1 <i200634@nu.edu.pk>
2023-09-19 19:19:51 +0500

    Main code

--- py_test.py
+++ py_test.py
@@ -2,18 +2,18 @@ from app import app
 
 def test_add():
     with app.test_client() as client:
-        response = client.get('/add/4/2')
+        response = client.get('calc/add/4/2')
         assert response.get_data(as_text=True) == '6'
 
 def test_div():
     with app.test_client() as client:
-        response = client.get('/div/6/3')
+        response = client.get('calc/div/6/3')
         assert response.get_data(as_text=True) == '2.0'
 
 def test_multiply():
     with app.test_client() as client:
-        response = client.get('/mul/2/3')
+        response = client.get('calc/mul/2/3')
         assert response.get_data(as_text=True) == '6'
 
-        response = client.get('/mul/1/2')
+        response = client.get('calc/mul/1/2')
         assert response.get_data(as_text=True) == '2'
